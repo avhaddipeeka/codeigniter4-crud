f659040ea11c02632ecf272241df0c612be49e36
avhaddipeeka <44333147+avhaddipeeka@users.noreply.github.com>
2025-12-09 22:42:47 +0530

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/codeigniter4/public/';
+    public string $baseURL = 'http://localhost/myproject/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
